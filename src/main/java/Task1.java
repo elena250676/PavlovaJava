2c92d6450715305d246825d5cc382e25e68c75b6
elena250676 <pavlovae434@gmail.com>
2023-11-15 10:15:50 +0300

    executed tasks

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Task1 {
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number>>>   ");
+        int number = scanner.nextInt();
+        if (number > 7) {
+            System.out.println("Privet");
+        }
+        name("vyacheslav");
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        array(arr);
+    }
+
+    public static void name(String name) {
+        if (name.equals("Vyacheslav")) {
+            System.out.println("Privet");
+        } else {
+            System.out.println("There is no such name");
+        }
+    }
+
+    public static void array(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 3 == 0) {
+                {
+                    System.out.println(arr[i] + " ");
+                }
+            }
+        }
+    }
 }
+
+//
+//    task2
+//            Дана скобочная последовательность: [((())()(())]]
+//            - Можно ли считать эту последовательность правильной?
+//            - Если ответ на предыдущий вопрос “нет”, то что необходимо в ней изменить, чтоб она стала правильной?
+//            no.
+//            the correct sequence would be as follows:
+//            [[((()))()(())]]
+
+//
